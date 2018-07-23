6ddaaa063ff6cf46b38c99c26e3b2cf1c4b8571b
Raouf Badjadja <36617115+eskaflon@users.noreply.github.com>
2018-07-23 13:01:10 +0900

    Add files via upload

---
+++ sketch_jul19a.ino
@@ -0,0 +1,39 @@
+#include <Bitcraze_PMW3901.h>
+#include <Wire.h>
+#include <VL53L0X.h>
+
+VL53L0X rangeSensor;
+
+// Using digital pin 10 for chip select
+Bitcraze_PMW3901 flow(10);
+
+void setup() {
+  Serial.begin(9600);
+
+  // Initialize flow sensor
+  if (!flow.begin()) {
+    Serial.println("Initialization of the flow sensor failed");
+    while(1) { }
+  }
+
+  // Initialize range sensor
+  Wire.begin();
+  rangeSensor.init();
+  rangeSensor.setTimeout(500);
+}
+
+int16_t deltaX,deltaY;
+
+void loop() {
+  // Get motion count since last call
+  flow.readMotionCount(&deltaX, &deltaY);
+
+  // Get single range measurement
+  float range = rangeSensor.readRangeSingleMillimeters();
+
+  Serial.println(deltaX);
+  Serial.println(deltaY);
+
+
+  delay(300);
+}
